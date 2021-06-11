a0d56cdebb29d5299bbba36d7f990e8649e8fa49
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-11 14:17:50 -0300

    termine el 2do parcial

--- 2DO PARCIAL 2020/main.c
+++ 2DO PARCIAL 2020/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define max 20
 typedef struct nodo{
     unsigned int dato;
     struct nodo *izq,*der;
@@ -33,4 +34,54 @@ int menorClave(TArbol A,int G,int N,int nAct){ //nAct entra con 1(nivel de la ra
     }
 }
 
+/*(Utilizar TDA N-ario) Dado un árbol N-Ario de enteros, verificar
+mediante una funcion int, que existe al menos un nodo de grado G
+(distinto de 0 y dato de entrada) que posea mas de la mitad de sus
+hijos con clave negativa*/
 
+int mitadNegativos(TNArio A, TPosicion p, int G){ //A, raiz(A),G
+    int gradoLocal=0,negativos=0,flag=0;TPosicion aux;
+    if (!nulo(p)){
+        aux = hijoMasIzq(p,A);
+        while (!nulo(aux) && !mitadNegativos(A,aux,G)){
+            gradoLocal++;
+            if (info(aux,A)<0)
+                negativos++;
+            aux=hnoDer(aux,A);
+        }
+        return (!nulo(aux)||(gradoLocal==G && negativos>gradoLocal/2));
+    }else
+        return 0;
+}
+/*Dado un digrafo de N vértices con aristas ponderadas almacenado
+en una matriz de adyacencia, hallar el porcentaje de vertices que teniendo
+bucle poseen el grado máximo de salida (RECORRIDO RECURSIVO)*/
+void maxGradoBucle(int matriz[][max],int i,int j,int orden,int *gMax,int gAct,int *bucles){ //i y j entran con orden-1, gMax es una variable inicializada en 0, gAct entra con 0
+    if (i>0) //mientras no me salga por arriba de la matriz
+        if (j!=0){ // si no estoy en el principio de la fila
+            if (matriz[i][j])
+                maxGradoBucle(matriz,i,j-1,orden,gMax,gAct+1,bucles);
+            else
+                maxGradoBucle(matriz,i,j-1,orden,gMax,gAct,bucles);
+        }else{ //j==0 FIN DE FILA
+            if (matriz[i][j] && gAct+1>*gMax){ //nuevo maximo con matriz[i][0]!=0
+                (*gMax)=gAct+1; //entonces al gMax es igual al acumulado mas 1
+                if (matriz[i][i]) //pregunto si existe el bucle para este nuevo maximo
+                    (*bucles)=1;
+                else
+                    (*bucles)=0;
+            }else{
+                if (gAct>*gMax){//nuevo maximo con matriz[i][0]==0
+                    (*gMax)=gAct; //gMax es el acumulado
+                    if (matriz[i][i])
+                        (*bucles)=1;
+                    else
+                        (*bucles)=0;
+                }
+                if ((matriz[i][j] && gAct+1==*gMax)||(gAct==*gMax)) //si el grado del vertice i == grMax 
+                    if (matriz[i][i]) //esto se podria juntar en el if de arriba pero quita legibilidad
+                        (*bucles++); //cuento el bucle
+            }
+            maxGradoBucle(matriz,i-1,orden-1,orden,gMax,gAct,bucles); //comienzo con la fila siguiente
+        }
+}
