b619b776b86e2103bfa264b53290d3fa0264b9c4
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-06-12 16:56:28 -0300

    parcial 1C 2019

--- 2P1C2019/main.c
+++ 2P1C2019/main.c
@@ -24,3 +24,49 @@ void claveMaxGr2(TArbol A,int nAct, int *nMax,int *clave){ //nAct entra con 1, n
         claveMaxGr2(A->izq,nAct,nMax,clave);
     }
 }
+
+char claveMaxGr2Char(TArbol A){
+    int izq,der;
+    if (A){
+        izq=claveMaxGr2Char(A->izq);
+        der=claveMaxGr2Char(A->der);
+        if (A->der && A->izq && izq==' ' && der==' ') //si es de grado 2 y no encontre uno mayor
+            return A->dato;
+        else{ //si es de grado 1 o encontre uno mayor
+            if (izq!=' ')
+                return izq;
+            else
+                if (der!=' ')
+                    return der; //devuelvo el que haya sido mayor
+                else //si es de grado 1 y no hay ninguno mayor devuelvo caracter vacio
+                    return ' ';
+        }
+    }else
+        return ' ';
+}
+
+/*TDA N-Ario: Dado un árbol N-Ario de enteros, desrrollar una funcion int
+que obtenga la cantidad de nodos con grado mayor a 1 entre los niveles k1 y k2, 
+que verifican que las claves del primer y ultimo hijo son mayores al nivel del nodo*/
+
+int loco(TNArio A, TPosicion p,int k1,int k2,int nAct){
+    int gradoLocal=0; TPosicion aux; int pri,ult;
+    if (!nulo(p)&&nAct<=k2){
+        if (nAct<k1)
+            return loco(A,hijoIzq(p,A),k1,k2,nAct+1)+loco(A,hnoDer(p,A),k1,k2,nAct);
+        else{
+            aux = hijoIzq(p,A);
+            pri = info(aux,A);
+            while(!nulo(aux)){
+                gradoLocal++;
+                ult=info(aux,A);
+                aux=hnoDer(aux,A);
+            }
+            if (gradoLocal>1 && pri>nAct && ult>nAct)
+                return 1+loco(A,hijoIzq(p,A),k1,k2,nAct+1)+loco(A,hnoDer(p,A),k1,k2,nAct);
+            else
+                return loco(A,hijoIzq(p,A),k1,k2,nAct+1)+loco(A,hnoDer(p,A),k1,k2,nAct);
+        }
+    }else
+        return 0;
+}
